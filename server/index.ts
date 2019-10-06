00a548b3088fbca41fb61bd7c12085dcf6827756
Calvin Lee <clee421@gmail.com>
2019-10-06 17:28:05 -0400

    chore: remove console.logs as per linter

--- server/index.ts
+++ server/index.ts
@@ -12,13 +12,9 @@ function add(a: number, b: number): number {
 
 app.use(serve('./dist'));
 
-console.log('doing other stuff');
-
 router.get('/', async function (ctx, next) {
   await ctx.render('index')
 })
-
-console.log(add(1, 3));
 
 app.use(views(__dirname + '/templates', {
   extension: 'pug'
@@ -27,5 +23,3 @@ app.use(views(__dirname + '/templates', {
 app.use(router.routes());
 
 app.listen(3000);
-
-console.log('Server running on port 3000');
