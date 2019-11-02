72af812bd7af2c66458bd71faa080cde97fd3a8d
Calvin Lee <clee421@gmail.com>
2019-11-02 14:39:46 -0400

    feat: add authenticated routes

--- client/src/components/middleware/route.tsx
+++ client/src/components/middleware/route.tsx
@@ -1,12 +1,45 @@
 import React, { ReactElement } from 'react';
-import { Route, RouteProps } from 'react-router-dom';
+import { RouteChildrenProps } from 'react-router';
+import { Route, RouteProps, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const MKNRoute: React.FC<RouteProps> = (props: RouteProps): ReactElement => {
+// Types
+import { AppState } from '@store/index';
+type Children = ((props: RouteChildrenProps<unknown>) => React.ReactNode) | React.ReactNode;
+
+const LOGIN_PATH = '/login';
+
+const MKNRoute: React.FC<RouteProps> = ({children, ...otherProps}: RouteProps): ReactElement => {
+  // TODO: Refactor redirect logic
+  const token = useSelector((state: AppState): string => state.system.token);
+
+  let pathname: string;
+  let redirect = false;
+  if (token && otherProps.path === LOGIN_PATH) {
+    redirect = true;
+    pathname = '/';
+  } else if (!token && otherProps.path !== LOGIN_PATH) {
+    redirect = true;
+    pathname = LOGIN_PATH;
+  }
+  // END TODO
 
   return (
-    <Route {...props}>
-      {props.children}
-    </Route>
+    <Route
+      {...otherProps}
+      render={({ location }): Children => (
+        redirect ? (
+          <Redirect 
+            to={{
+              pathname,
+              state: { from: location },
+            }}
+          />
+        ) : (
+          children
+        )
+      )}
+    />
   );
 };
 
