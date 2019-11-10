5ac1a6f9ff18303dbdc494a3f657161f821a7b29
Calvin Lee <clee421@gmail.com>
2019-11-10 12:19:44 -0500

    tech: refactor root resolvers to read files for require

--- server/graphql/root.resolvers.ts
+++ server/graphql/root.resolvers.ts
@@ -1,7 +1,26 @@
-import { resolvers as authentication } from './authentication/authentication.resolvers';
-import { resolvers as campaign } from './campaign/campaign.resolvers';
+import { readdirSync, statSync } from 'fs';
+import { join } from 'path';
 
-export const resolvers = [
-  authentication,
-  campaign,
-];
+import { IResolvers } from 'apollo-server-koa';
+
+const PATH_TO_CURRENT_FILE = 'server/graphql';
+
+export const resolvers: IResolvers[] = [];
+
+(function(path: string): void {
+  const dirs = readdirSync(path).filter(
+    (file: string) => statSync(join(path, file)).isDirectory()
+  );
+
+  dirs.forEach((dir: string) => {
+    const files = readdirSync(path + '/' + dir);
+
+    files.forEach((file: string) => {
+      if (file.endsWith('.resolvers.ts')) {
+        /* eslint-disable-next-line @typescript-eslint/no-var-requires */
+        const td = require(`./${dir}/${file}`).resolvers;
+        resolvers.push(td);
+      }
+    });
+  });
+})(PATH_TO_CURRENT_FILE);
